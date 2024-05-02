ef9da8a9deb81ebe32c64a7dad112f5594bf0a20
Natalia <naticastecno@gmail.com>
2024-05-02 10:01:02 +0200

    vite project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+import "./App.css";
+import React from "react";
+import { Nav } from "./components/nav/nav";
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+};
+
+export default App;
